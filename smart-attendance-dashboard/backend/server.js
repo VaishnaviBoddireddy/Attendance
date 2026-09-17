8488bc8cecee19cfac8544d8bca9ab02b75f5e08
VAISHNAVI BODDDIREDDY <boddireddy100052000@gmail.com>
2026-09-17 16:01:06 +0530

    Set up Express server with routes and DB connection

--- smart-attendance-dashboard/backend/server.js
+++ smart-attendance-dashboard/backend/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/attendance', require('./routes/attendanceRoutes'));
+app.use('/api/analytics', require('./routes/analyticsRoutes'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
