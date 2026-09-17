b7820d6c5ebadabfb1a434525d89251010d52e7d
VAISHNAVI BODDDIREDDY <boddireddy100052000@gmail.com>
2026-09-17 21:19:28 +0530

    Add CommonModule to LoginComponent imports

--- smart-attendance-dashboard/frontend/src/app/components/login/login.component.ts
+++ smart-attendance-dashboard/frontend/src/app/components/login/login.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone:true,
-  imports:[FormsModule]
+  imports:[FormsModule,CommonModule]
 })
 export class LoginComponent {
   rollNumber = '';
