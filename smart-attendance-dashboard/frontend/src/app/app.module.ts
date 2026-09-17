0cc7c40d311930bcd0b207ad46028d5f0232a681
VAISHNAVI BODDDIREDDY <boddireddy100052000@gmail.com>
2026-09-17 16:12:49 +0530

    Update app.module.ts

--- smart-attendance-dashboard/frontend/src/app/app.module.ts
+++ smart-attendance-dashboard/frontend/src/app/app.module.ts
@@ -1,1 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgChartsModule } from 'ng2-charts';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { QrScannerComponent } from './components/qr-scanner/qr-scanner.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    QrScannerComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
